ca28d451756c7c300fa6bc103861aff240088a51
Amandubey12 <76807303+Amandubey12@users.noreply.github.com>
2021-11-12 23:12:40 +0530

    upadate

---
+++ logic.js
@@ -0,0 +1,32 @@
+const MyFunction1 = () => {
+    document.getElementById("first").style.display ='grid';
+    document.getElementById("second").style.display ='none';
+    document.getElementById("third").style.display ='none';
+    document.getElementById("product").innerHTML="Poduct categary FOOD"
+          }
+          const MyFunction2 = () => {
+            document.getElementById("first").style.display ='none';
+            document.getElementById("second").style.display ='grid';
+            document.getElementById("third").style.display ='none';
+            document.getElementById("product").innerHTML="Poduct categary Electronics"
+                  }
+                  const MyFunction3 = () => {
+                    document.getElementById("first").style.display ='none';
+                    document.getElementById("second").style.display ='none';
+                    document.getElementById("third").style.display ='grid';
+                    document.getElementById("product").innerHTML="Poduct categary Cloths"
+                          }
+                           
+                                 
+                          const addToCart = document.getElementsByClassName('add-to-cart');
+                          const productRow = document.getElementsByClassName('product-row');
+                          
+                          for (var i = 0; i < addToCart.length; i++) {
+                            button = addToCart[i];
+                            button.addEventListener('click', addToCartClicked)
+                          }
+                          
+                          
+                          
+                          
+                          
